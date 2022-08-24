84cd70c6197277fb4bd0654325b0502339000c66
sadman-sa <69650510+sadman-sa@users.noreply.github.com>
2022-08-24 12:13:30 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const io = require('socket.io')(3000)
+var express = require('express');
+var app = express();
 
-const users = {}
+const server = require('http').createServer(app)
+server.listen(process.env.PORT || 3001, ()=> {
+    console.log("server running...")
+  })
 
-io.on('connection', socket => {
-  socket.on('new-user', name => {
-    users[socket.id] = name
-    socket.broadcast.emit('user-connected', name)
-  })
-  socket.on('send-chat-message', message => {
-    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
-  })
-  socket.on('disconnect', () => {
-    socket.broadcast.emit('user-disconnected', users[socket.id])
-    delete users[socket.id]
-  })
-})
+app.get('/',function(req, res){
+    res.sendFile('home.html',{root:__dirname})
+});
+
+const io = require('socket.io')(server);
+io.on('connection', client => {
+    console.log("api connected..")
+    });
