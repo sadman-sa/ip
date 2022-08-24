954940116e44e6e1e93f93a814abdad4966dafae
sadman-sa <69650510+sadman-sa@users.noreply.github.com>
2022-08-24 15:10:51 +0600

    const port = process.env.PORT || 8000;

--- server.js
+++ server.js
@@ -3,14 +3,24 @@ const app = express()
 const server = require("http").createServer(app)
 const io = require('socket.io')(server, {cors: {origin:"*"}})
 
+const port = process.env.PORT || 8000;
+
 app.get('/',function(req, res){
     res.sendFile('index.html',{root:__dirname})
 });
-server.listen(3000, ()=>{
-    console.log("server running");
-});
+
 io.on("connection", (socket)=>{
+    socket.on("IP", (data)=>{
+        console.log(data)
+        socket.broadcast.emit('show_ip', data )
+    })
     socket.on("message", (data)=>{
         console.log(data)
     })
 })
+
+
+
+server.listen(port, ()=>{
+    console.log("server running");
+});
