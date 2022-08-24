d3436ddb82f19ef1951c8108f68d1f34b6a94bf9
sadman-sa <69650510+sadman-sa@users.noreply.github.com>
2022-08-24 11:56:42 +0600

    asdadasdsad

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-//const socket = io('http://localhost:3000')
-const server = require('http').createServer(app)
-const socket = require('socket.io')(server,{cors:{origin: "*" }})
+const socket = io('http://localhost:3000')
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
